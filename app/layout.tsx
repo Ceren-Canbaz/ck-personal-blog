4e8165353730730ca193253cb97662ee1dce89f9
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2024-08-09 15:05:08 +0300

    ui:new font added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter , Fira_Code} from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Provider } from "./utils/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
+const firaCode = Fira_Code({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+            <body className={`${firaCode.className} bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50`}>
         <Provider>
         <Navbar />
         <main className='mx-auto max-w-5xl px-6'>
